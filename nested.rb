793923cdda6211913ac84a77e0907be2941dc586
Johnathan  Guzman <johnathanguzman17@gmail.com>
2020-03-11 03:00:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,6 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
+		programmer_hash[:yukihiro_matsumoto ] = {
+			:known_for => "Ruby",
+			:languages => ["LISP", "C"]
+		}
+	programmer_hash
 
 end
 
@@ -103,11 +108,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-		programmer_hash[:yukihiro_matsumoto ] = {
-			:known_for => "Ruby",
-			:languages => ["LISP", "C"]
-		}
-	programmer_hash
+		
 end
 
 def adding_to_dennis
